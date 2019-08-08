59f23f1f07627324f4b18a916f183b0d8c14b7de
Mirza S Reza <mirzasreza@gmail.com>
2019-08-09 00:19:05 +0600

    conditional formatting

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
         image: './assets/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inStock: true,
+        onSale: true
     }
 })
